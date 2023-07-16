c19be4b375a47ad1cd296afb1cdd2d226ac4b8bd
ramadhantriyant <me@ramadhantriyant.id>
2023-07-16 21:27:24 +0700

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,33 @@
+export default {
+	async fetch(request, env) {
+		const telegramUrl = `https://api.telegram.org/bot${env.TOKEN}/sendMessage`;
+
+		// Set HTTP Headers
+		const headers = {
+			'content-type': 'application/json',
+			'accept': 'application/json'
+		}
+
+		// Read message sent from Telegram
+		const body = await request.json();
+		const chatId = body.message.from.id;
+		const text = body.message.text;
+
+		// Send Message to Telegram
+		const response = await fetch(telegramUrl, {
+			method: 'POST',
+			headers: headers,
+			body: JSON.stringify({
+				chat_id: chatId,
+				text: text
+			})
+		});
+
+		// Reply Response to Telegram, should be unneeded
+		async function getResponse(response) {
+			return JSON.stringify(await response.json());
+		}
+		const results = await getResponse(response);
+		return new Response(JSON.stringify(results), { headers: headers });
+	},
+};
